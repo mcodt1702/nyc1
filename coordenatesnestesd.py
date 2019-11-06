482f2898afcc34b823154ec13ce0d8b9adea8a25
mcodt1702 <57457274+mcodt1702@users.noreply.github.com>
2019-11-06 16:08:35 -0500

    Add files via upload

---
+++ coordenatesnestesd.py
@@ -0,0 +1,6 @@
+numbers = [2,1,2,9,6]
+max = 0
+for number in numbers:
+    if max < number:
+        max = number
+print(max)
---
+++ total.py
@@ -0,0 +1,5 @@
+total = 0
+prices = [10,20,30]
+for item in prices:
+    total = total + item
+print(f"Total: {total}")
